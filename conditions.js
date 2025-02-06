4de51757fa074dfee9ef517a5cd1e862202f30fe
PereMarquez-web <pere.marquez@icloud.com>
2025-02-06 15:53:45 +0000

    starting javascript

---
+++ conditions.js
@@ -0,0 +1,14 @@
+var age = 3;
+switch (age) {
+    case 1:
+        console.log("You are in year 1");
+        break;
+    case 2:
+        console.log("You are in year 2");
+        break;
+    case 3:
+        console.log("You are in year 3");
+        break;
+    default:
+      // block of code to run when otherwise
+  }
